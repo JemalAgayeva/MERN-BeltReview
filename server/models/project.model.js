bdea52911b28568186471a8c9cec59a89f030606
Jemal Agayeva <Jemal.agayeva@gmail.com>
2021-03-24 11:21:07 -0400

    backend is set

---
+++ server/models/project.model.js
@@ -0,0 +1,23 @@
+
+const mongoose = require('mongoose');
+
+// Basic setup of the Mongoose Schema
+const ProjectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required:[true,"Name is required"],
+        minlength: [3,"Name has to be not less than 3 characters"]
+    },
+    description: {
+        required:[true,"Description is required"],
+        type: String
+    },
+    // punchline: String
+})
+
+
+// This is how we register our schema.
+const Project = mongoose.model("Project", ProjectSchema);
+
+// Finally we export it out of the file.
+module.exports = Project;
